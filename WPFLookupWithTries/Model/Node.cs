54ac95489aa9e1c08feae7ebc58c93921494ff88
shekhar249 <chandrashekhar.it@gmail.com>
2018-06-15 21:17:37 -0400

    Initial commit

---
+++ WPFLookupWithTries/Model/Node.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+
+namespace WPFLookupWithTries.Model
+{
+    class Node
+    {
+        public Dictionary<char, Node> map;
+        bool isCompleteString;
+        int id;
+        public Node()
+        {
+            map = new Dictionary<char, Node>();
+            isCompleteString = false;
+        }
+
+        public void AddContact(string contact,int id)
+        {
+            Node temp = this;
+            for (int i = 0; i < contact.Length; i++)
+            {
+                char c = contact[i];
+
+                if (!temp.map.ContainsKey(c))
+                {
+                    Node newNode = new Node();
+                    temp.map.Add(c, newNode);
+                    temp = newNode;
+                    if (i == contact.Length - 1)
+                    {
+                        temp.isCompleteString = true;
+                        temp.id = id;
+                    }
+                }
+                else
+                {
+                    temp = temp.map[c];
+                }
+            }
+        }
+        public List<int> FindContactIds(string partial)
+        {
+            Node temp = this;
+            for (int i = 0; i < partial.Length; i++)
+            {
+                char c = partial[i];
+                if (temp.map.ContainsKey(c))
+                {
+                    temp = temp.map[c];
+                }
+                else
+                {
+                    return new List<int>();
+                }
+            }
+            return TraverseTreeToCount(temp);
+        }
+        List<int> TraverseTreeToCount(Node n)
+        {
+            if (n == null)
+                return new List<int>();
+            List<int> ids = new List<int>();
+            if (n.isCompleteString)
+                ids.Add(n.id);
+            foreach (var kvmap in n.map)
+            {
+                var result = TraverseTreeToCount(kvmap.Value);
+                if (result.Count > 0)
+                {
+                    ids.AddRange(result);
+                }
+            }
+            return ids;
+        }
+
+        public Dictionary<int, string> LoadDummyContacts()
+        {
+            Dictionary<int, string> contacts = new Dictionary<int, string>();
+
+            contacts.Add(1,"chan");
+            contacts.Add(2,"chandra");
+            contacts.Add(3,"chandrash");
+            contacts.Add(4,"chandrashekhar");
+            contacts.Add(5,"she");
+            contacts.Add(6,"shek");
+            contacts.Add(7,"shekhar");
+            contacts.Add(8,"avi");
+            contacts.Add(9,"avish");
+            contacts.Add(10,"avishka");
+            contacts.Add(11, "mo");
+            contacts.Add(12, "mon");
+            contacts.Add(13, "moni");
+            contacts.Add(14, "mona");
+            contacts.Add(15, "monu");
+            contacts.Add(16, "monal");
+            contacts.Add(17, "monali");
+            foreach (var item in contacts)
+            {
+                this.AddContact(item.Value,item.Key);
+
+            }
+            return contacts;
+        }
+
+    }
+
+}
---
+++ WPFLookupWithTries/ViewModel/TriesViewModel.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFLookupWithTries.Model;
+
+namespace WPFLookupWithTries.ViewModel
+{
+    class TriesViewModel : INotifyPropertyChanged
+    {
+        Node triesRoot;
+        Dictionary<int, string> map;
+        public TriesViewModel()
+        {
+            LoadContacts();
+          
+        }
+
+        async void LoadContacts()
+        {
+            await Task.Run(
+                () =>
+                {
+                    triesRoot = new Node();
+                    //uncomment below line to have few records to filter
+                    //map = n.LoadDummyContacts();
+                    // Read the file and add it line by line as a contact.  
+                    System.IO.StreamReader file =
+                        new System.IO.StreamReader("Model\\contacts.txt");
+                    string line;
+                    map = new Dictionary<int, string>();
+                    int counter = 1;
+                    while ((line = file.ReadLine()) != null)
+                    {
+                        map.Add(counter, line);
+                        triesRoot.AddContact(line, counter++);
+                    }
+
+                    file.Close();
+                    Contacts = new ObservableCollection<string>(map.Values.ToList());
+                    RecordsCount = string.Format("Result count :{0}", Contacts.Count);
+                }
+            );
+        }
+
+        string contactPartial;
+        public string ContactPartial
+        {
+            get
+            {
+                return contactPartial;
+            }
+
+            set
+            {
+                contactPartial = value;
+                RaisePropertyChanged("ContactPartial");
+                BindContacts();
+            }
+        }
+        string recordsCount;
+        public string RecordsCount
+        {
+            get
+            {
+                return recordsCount;
+            }
+
+            set
+            {
+                recordsCount = value;
+                RaisePropertyChanged("RecordsCount");
+
+            }
+        }
+
+
+        ObservableCollection<string> contacts = new ObservableCollection<string>();
+
+        public ObservableCollection<string> Contacts
+        {
+            get
+            {
+                return contacts;
+            }
+            set
+            {
+                contacts = value;
+                RaisePropertyChanged("Contacts");
+            }
+        }
+        async void BindContacts()
+        {
+            await Task.Run(() =>
+           {
+               List<int> filtered = triesRoot.FindContactIds(ContactPartial);
+               List<string> filteredRecords = new List<string>();
+               foreach (var item in map)
+               {
+                   if (filtered.Contains(item.Key))
+                   {
+                       filteredRecords.Add(item.Value);
+                   }
+               }
+               Contacts = new ObservableCollection<string>(filteredRecords);
+               RecordsCount = string.Format("Result count :{0}", filteredRecords.Count);
+           });
+
+        }
+
+        void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+    }
+}
